3c3caa4a98f89ed9d883a457d25af45e727fbd93
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-03-12 23:17:46 +0700

    listView

--- Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
+++ Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ex_6_listviewdemo;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> dsl;
+        dsl = new ArrayList<String>();
+        dsl.add("Chúng ta của tương lai");
+        dsl.add("Chúng ta của hiện tại");
+        dsl.add("Chúng ta của sau này");
+        ArrayAdapter<String> adapterlist;
+        adapterlist = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsl);
+        ListView lv= findViewById(R.id.ds);
+        lv.setAdapter(adapterlist);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
