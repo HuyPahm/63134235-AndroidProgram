42dc5afba6fa9c3b393dc229d8a0941d63ee0c8d
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-03-13 00:33:37 +0700

    thêm 1 bộ xử lý sự kiện

--- Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
+++ Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ex_6_listviewdemo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,7 +33,19 @@ public class MainActivity extends AppCompatActivity {
         adapterlist = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsl);
         ListView lv= findViewById(R.id.ds);
         lv.setAdapter(adapterlist);
+//tạo bộ lắng nghe
 
+        lv.setOnItemClickListener(bolangnghevaxuly);
+        AdapterView.OnItemClickListener bolangnghevaxuly = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+
+                //tạo thong báo
+                Toast.makeText(MainActivity.this,"ghê chưa"+ String.valueOf(i), Toast.LENGTH_LONG).show();
+
+
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
