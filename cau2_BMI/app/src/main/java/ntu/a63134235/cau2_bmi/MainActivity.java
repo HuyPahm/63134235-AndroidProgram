8a24135c96e179076b0d58f7fa06b7c13acb1173
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-03-25 22:39:06 +0700

    bmi

---
+++ cau2_BMI/app/src/main/java/ntu/a63134235/cau2_bmi/MainActivity.java
@@ -0,0 +1,35 @@
+package ntu.a63134235.cau2_bmi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText heightInput;
+    private EditText weightInput;
+    private Button calculateButton;
+    private TextView resultText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        heightInput = findViewById(R.id.heightInput);
+        weightInput = findViewById(R.id.weightInput);
+        calculateButton = findViewById(R.id.calculateButton);
+        resultText = findViewById(R.id.resultText);
+
+
+    }
+}
