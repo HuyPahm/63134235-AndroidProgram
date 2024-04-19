4221738ecbeab17a6b585817c5c1863e33c186b6
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-04-19 11:24:03 +0700

    error

---
+++ RecylerView_Exam/app/src/main/java/ntu/a63134235/recylerview_exam/examViewHolder.java
@@ -0,0 +1,29 @@
+// ViewHolder code for RecyclerView
+package com.example.admin.example;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class examViewHolder
+        extends RecyclerView.ViewHolder {
+    TextView examName;
+    TextView examMessage;
+    TextView examDate;
+    View view;
+
+    examViewHolder(View itemView)
+    {
+        super(itemView);
+        examName
+                = (TextView)itemView
+                .findViewById(R.id.examName);
+        examDate
+                = (TextView)itemView
+                .findViewById(R.id.examDate);
+        examMessage
+                = (TextView)itemView
+                .findViewById(R.id.examMessage);
+        view = itemView
+    }
+}
